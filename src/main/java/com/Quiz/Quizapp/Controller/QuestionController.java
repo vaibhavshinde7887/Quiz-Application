75e7a4a13f16aad3fd842a79f79d71c664d48d0a
vaibhavshinde7887 <vshinde7887@gmail.com>
2025-03-02 01:57:55 +0530

    Questio pattern Design Done

--- src/main/java/com/Quiz/Quizapp/Controller/QuestionController.java
+++ src/main/java/com/Quiz/Quizapp/Controller/QuestionController.java
@@ -1,11 +1,14 @@
 package com.Quiz.Quizapp.Controller;
 
 
+import com.Quiz.Quizapp.Entity.Question;
 import com.Quiz.Quizapp.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 
@@ -16,9 +19,20 @@ public class QuestionController {
     QuestionService questionService;
 
    @GetMapping("/allQuestions")
-   public String getAllQuestions(){
-
+   public ResponseEntity<List<Question>> getAllQuestions(){
        return questionService.getAllQuestions();
 
    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+
+       return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+
+    }
 }
---
+++ src/main/java/com/Quiz/Quizapp/DaoRepository/QuestionDao.java
@@ -0,0 +1,15 @@
+package com.Quiz.Quizapp.DaoRepository;
+
+
+import com.Quiz.Quizapp.Entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+}
